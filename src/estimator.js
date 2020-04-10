083bbac13a964015613dbdf09f1e0a098e310692
sonylomo <49971500+sonylomo@users.noreply.github.com>
2020-04-10 17:04:45 +0300

    without eslint-styling

--- src/estimator.js
+++ src/estimator.js
@@ -1,93 +1,75 @@
-var input = {
-        region: { 
-            name: "Africa",
-            avgAge: 19.7,
-            avgDailyIncomeInUSD: 5,
-            avgDailyIncomePopulation: 0.71 },
-        periodType: "days",
-        timeToElapse: 58,
-        reportedCases: 674,
-        population: 66622705,
-        totalHospitalBeds: 1380614
-    }
-
-var output = 
-    {
-        data: {
-            region: { 
-                name: "Africa",
-                avgAge: 19.7,
-                avgDailyIncomeInUSD: 5,
-                avgDailyIncomePopulation: 0.71 },
-            periodType: "days",
-            timeToElapse: 58,
-            reportedCases: 674,
-            population: 66622705,
-            totalHospitalBeds: 1380614
-        },          // the input data you got
-        impact: {
-            currentlyInfected: 0,
-            infectionsByRequestedTime: 0,
-            severeCasesByRequestedTime: 0,
-            hospitalBedsByRequestedTime: 0,
-            casesForICUByRequestedTime: 0,
-            casesForVentilatorsByRequestedTime: 0,
-            dollarsInFlight:0
-        },        // your best case estimation
-        severeImpact: {
-            currentlyInfected: 0,
-            infectionsByRequestedTime: 0,
-            severeCasesByRequestedTime: 0,
-            hospitalBedsByRequestedTime: 0,
-            casesForICUByRequestedTime: 0,
-            casesForVentilatorsByRequestedTime: 0,
-            dollarsInFlight:0
-        }   // your severe case estimation
-     }; 
-
-const covid19ImpactEstimator = (data) => {
-    //output data{} = input data
-    output.data = data;
-
-    //(severe) impact reported cases
-    var casesReported = data.reportedCases;
-    output.impact.currentlyInfected = casesReported * 10;
-    output.severeImpact.currentlyInfected = casesReported * 50;
-
-    //estimate no. of infected in 28 days
-    var factor = data.timeToElapse / 3;
-    var impactInfectionsByTime = output.impact.infectionsByRequestedTime;
-    var severeInfectionsByTime = output.severeImpact.infectionsByRequestedTime;
-
-    impactInfectionsByTime = output.impact.currentlyInfected * 2 ^ factor;
-    severeInfectionsByTime = output.severeImpact.currentlyInfected * 2 ^ factor;
-
-    //require hospitalisation
-    output.impact.severeCasesByRequestedTime = output.impact.infectionsByRequestedTime * 0.15;
-    output.severeImpact.severeCasesByRequestedTime = output.severeImpact.infectionsByRequestedTime * 0.15;
-
-    //available beds
-    var severeImpactCases = output.impact.severeCasesByRequestedTime;
-    output.impact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - severeImpactCases;
-
-    var severeCases = output.severeImpact.severeCasesByRequestedTime;
-    output.severeImpact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - severeCases;
- 
-    //require ICU care
-    output.impact.casesForICUByRequestedTime = impactInfectionsByTime * 0.05;
-    output.severeImpact.casesForICUByRequestedTime = severeInfectionsByTime * 0.05;
-
-    //need ventilators in the ICU
-    output.impact.casesForVentilatorsByRequestedTime = impactInfectionsByTime * 0.02;
-    output.severeImpact.casesForVentilatorsByRequestedTime = severeInfectionsByTime * 0.02;
-
-    //money lost in the economy
-    var dailyPopIncome = data.region.avgDailyIncomePopulation;
-    var dailyDollarIncome = data.region.avgDailyIncomeInUSD;
-
-    output.impact.dollarsInFlight = impactInfectionsByTime * dailyPopIncome * dailyDollarIncome * data.timeToElapse;
-    output.severeImpact.dollarsInFlight = severeInfectionsByTime * dailyPopIncome * dailyDollarIncome * data.timeToElapse;
+/* eslint-disable linebreak-style */
+const input = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
 };
 
-covid19ImpactEstimator(input);
+const output = {
+  data: input, // the input data you got
+  impact: {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0,
+    severeCasesByRequestedTime: 0,
+    hospitalBedsByRequestedTime: 0,
+    casesForICUByRequestedTime: 0,
+    casesForVentilatorsByRequestedTime: 0,
+    dollarsInFlight: 0
+  }, // your best case estimation
+  severeImpact: {
+    currentlyInfected: 0,
+    infectionsByRequestedTime: 0,
+    severeCasesByRequestedTime: 0,
+    hospitalBedsByRequestedTime: 0,
+    casesForICUByRequestedTime: 0,
+    casesForVentilatorsByRequestedTime: 0,
+    dollarsInFlight: 0
+  } // your severe case estimation
+};
+
+const covid19ImpactEstimator = (data) => {
+  // (severe) impact reported cases
+  const casesReported = data.reportedCases;
+  output.impact.currentlyInfected = casesReported * 10;
+  output.severeImpact.currentlyInfected = casesReported * 50;
+  // estimate no. of infected in 28 days
+  const factor = data.timeToElapse / 3;
+  let impactInfectionsByTime = output.impact.infectionsByRequestedTime;
+  let severeInfectionsByTime = output.severeImpact.infectionsByRequestedTime;
+  impactInfectionsByTime = output.impact.currentlyInfected * 2 ** factor;
+  severeInfectionsByTime = output.severeImpact.currentlyInfected * 2 ** factor;
+  // require hospitalisation
+  const numOfSevereInfections = output.severeImpact.infectionsByRequestedTime;
+  output.impact.severeCasesByRequestedTime = output.impact.infectionsByRequestedTime * 0.15;
+  output.severeImpact.severeCasesByRequestedTime = numOfSevereInfections * 0.15;
+  // available beds
+  const severeImpactCases = output.impact.severeCasesByRequestedTime;
+  output.impact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - severeImpactCases;
+  const severeCases = output.severeImpact.severeCasesByRequestedTime;
+  output.severeImpact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - severeCases;
+  // require ICU care
+  output.impact.casesForICUByRequestedTime = impactInfectionsByTime * 0.05;
+  output.severeImpact.casesForICUByRequestedTime = severeInfectionsByTime * 0.05;
+
+  // need ventilators in the ICU
+  output.impact.casesForVentilatorsByRequestedTime = impactInfectionsByTime * 0.02;
+  output.severeImpact.casesForVentilatorsByRequestedTime = severeInfectionsByTime * 0.02;
+
+  // money lost in the economy
+  const popInc = data.region.avgDailyIncomePopulation;
+  const dInc = data.region.avgDailyIncomeInUSD;
+  output.impact.dollarsInFlight = impactInfectionsByTime * popInc * dInc * data.timeToElapse;
+  output.severeImpact.dollarsInFlight = severeInfectionsByTime * popInc * dInc * data.timeToElapse;
+
+  return output;
+};
+
 export default covid19ImpactEstimator;

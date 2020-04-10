67bd3e18c0f00bc9729933859d3e04058d732292
sonylomo <49971500+sonylomo@users.noreply.github.com>
2020-04-10 18:56:15 +0300

    new changes

--- src/estimator.js
+++ src/estimator.js
@@ -13,57 +13,62 @@ const input = {
   totalHospitalBeds: 1380614
 };
 
-const output = {
-  data: input, // the input data you got
-  impact: {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0,
-    severeCasesByRequestedTime: 0,
-    hospitalBedsByRequestedTime: 0,
-    casesForICUByRequestedTime: 0,
-    casesForVentilatorsByRequestedTime: 0,
-    dollarsInFlight: 0
-  }, // your best case estimation
-  severeImpact: {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0,
-    severeCasesByRequestedTime: 0,
-    hospitalBedsByRequestedTime: 0,
-    casesForICUByRequestedTime: 0,
-    casesForVentilatorsByRequestedTime: 0,
-    dollarsInFlight: 0
-  } // your severe case estimation
-};
 
 const covid19ImpactEstimator = (data) => {
+  const output = {
+    data: input, // the input data you got
+    impact: {
+      currentlyInfected: 0,
+      infectionsByRequestedTime: 0,
+      severeCasesByRequestedTime: 0,
+      hospitalBedsByRequestedTime: 0,
+      casesForICUByRequestedTime: 0,
+      casesForVentilatorsByRequestedTime: 0,
+      dollarsInFlight: 0
+    }, // your best case estimation
+    severeImpact: {
+      currentlyInfected: 0,
+      infectionsByRequestedTime: 0,
+      severeCasesByRequestedTime: 0,
+      hospitalBedsByRequestedTime: 0,
+      casesForICUByRequestedTime: 0,
+      casesForVentilatorsByRequestedTime: 0,
+      dollarsInFlight: 0
+    } // your severe case estimation
+  };
+
+  // (severe) impact reported cases
+  const casesReported = data.reportedCases;
+  output.impact.currentlyInfected = casesReported * 10;
+  output.severeImpact.currentlyInfected = casesReported * 50;
+
   // convert period-type to days
   if (data.periodType === 'weeks') {
     data.timeToElapse *= 7;
   }
   if (data.periodType === 'months') {
     data.timeToElapse *= 30;
+  } else {
+    // estimate no. of infected in 28 days
+    const fa = data.timeToElapse / 3;
+    output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * 2 ** fa;
+    output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * 2 ** fa;
   }
 
-  // (severe) impact reported cases
-  const casesReported = data.reportedCases;
-  output.impact.currentlyInfected = casesReported * 10;
-  output.severeImpact.currentlyInfected = casesReported * 50;
-  // estimate no. of infected in 28 days
-  const factor = data.timeToElapse / 3;
-  let impactInfectionsByTime = output.impact.infectionsByRequestedTime;
-  let severeInfectionsByTime = output.severeImpact.infectionsByRequestedTime;
-  impactInfectionsByTime = output.impact.currentlyInfected * 2 ** factor;
-  severeInfectionsByTime = output.severeImpact.currentlyInfected * 2 ** factor;
   // require hospitalisation
   const numOfSevereInfections = output.severeImpact.infectionsByRequestedTime;
   output.impact.severeCasesByRequestedTime = output.impact.infectionsByRequestedTime * 0.15;
   output.severeImpact.severeCasesByRequestedTime = numOfSevereInfections * 0.15;
+
   // available beds
   const severeImpactCases = output.impact.severeCasesByRequestedTime;
   output.impact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - severeImpactCases;
   const severeCases = output.severeImpact.severeCasesByRequestedTime;
   output.severeImpact.hospitalBedsByRequestedTime = data.totalHospitalBeds * 0.35 - severeCases;
+
   // require ICU care
+  const impactInfectionsByTime = output.impact.infectionsByRequestedTime;
+  const severeInfectionsByTime = output.severeImpact.infectionsByRequestedTime;
   output.impact.casesForICUByRequestedTime = impactInfectionsByTime * 0.05;
   output.severeImpact.casesForICUByRequestedTime = severeInfectionsByTime * 0.05;
 
